a7c93d8c74c2328701022e8291e9107279f349fd
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2023-01-01 14:31:41 +0300

    v1.0.5

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-wget https://github.com/kaangiray26/vuejs-template/archive/refs/tags/v1.0.4.zip
+wget https://github.com/kaangiray26/vuejs-template/archive/refs/tags/v1.0.5.zip
 
-unzip v1.0.4.zip
+unzip v1.0.5.zip
 
-rm v1.0.4.zip
+rm v1.0.5.zip
 
-cp -a vuejs-template-1.0.4/. ./
+cp -a vuejs-template-1.0.5/. ./
 
-rm -rf vuejs-template-1.0.4
+rm -rf vuejs-template-1.0.5
 
 rm install.sh
 
