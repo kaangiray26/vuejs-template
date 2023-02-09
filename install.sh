f1d9728306cbaabf1e7bb928ca5b6e509e5c388f
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2023-02-09 10:06:23 +0100

    changed default branch name from master to main

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-wget https://github.com/kaangiray26/vuejs-template/archive/refs/tags/v1.0.5.zip
+wget https://github.com/kaangiray26/vuejs-template/archive/refs/tags/v1.0.6.zip
 
-unzip v1.0.5.zip
+unzip v1.0.6.zip
 
-rm v1.0.5.zip
+rm v1.0.6.zip
 
-cp -a vuejs-template-1.0.5/. ./
+cp -a vuejs-template-1.0.6/. ./
 
-rm -rf vuejs-template-1.0.5
+rm -rf vuejs-template-1.0.6
 
 rm install.sh
 
