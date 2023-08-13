77327052217bfd05e0dc55dc7c87795dfea65581
kaangiray26 <kaangiray26@protonmail.com>
2023-08-13 20:31:46 +0200

    updated packages

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-wget https://github.com/kaangiray26/vuejs-template/archive/refs/tags/v1.0.6.zip
+wget https://github.com/kaangiray26/vuejs-template/archive/refs/tags/v1.0.7.zip
 
-unzip v1.0.6.zip
+unzip v1.0.7.zip
 
-rm v1.0.6.zip
+rm v1.0.7.zip
 
-cp -a vuejs-template-1.0.6/. ./
+cp -a vuejs-template-1.0.7/. ./
 
-rm -rf vuejs-template-1.0.6
+rm -rf vuejs-template-1.0.7
 
 rm install.sh
 
